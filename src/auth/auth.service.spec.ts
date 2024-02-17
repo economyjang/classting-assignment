218212d872f4b8b567ed5babfed26bf804f21b40
Kyungjae-Jang <economyjang777@gmail.com>
2024-02-17 23:37:28 +0900

    feat: signup 컨트롤러, 서비스 유닛 테스트, esModuleInterop 옵션 설정

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/SignUpDto';
+import { UserType } from '../../types/UserType';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entity/User.entity';
+import { Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
+import { ConflictException } from '@nestjs/common';
 
 describe('AuthService', () => {
     let service: AuthService;
+    let userRepository: Repository<User>;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [AuthService],
+            providers: [
+                AuthService,
+                {
+                    provide: getRepositoryToken(User),
+                    useValue: {
+                        save: jest.fn(),
+                        exists: jest.fn(),
+                    },
+                },
+            ],
         }).compile();
 
         service = module.get<AuthService>(AuthService);
-    });
+        userRepository = module.get(getRepositoryToken(User));
 
+        jest.spyOn(bcrypt, 'hashSync').mockReturnValue('hashedPassword');
+    });
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    describe('signUpUser 테스트', () => {
+        it('정상 회원가입', async () => {
+            const signUpDto: SignUpDto = {
+                emailId: 'test@example.com',
+                password: 'password',
+                name: 'Test User',
+                type: UserType.STUDENT,
+            };
+
+            jest.spyOn(userRepository, 'exists').mockResolvedValue(false);
+            jest.spyOn(userRepository, 'save').mockResolvedValue(undefined);
+
+            await expect(service.signUpUser(signUpDto)).resolves.not.toThrow();
+            expect(userRepository.save).toHaveBeenCalledWith(expect.any(User));
+        });
+
+        it('이미 존재하는 사용자', async () => {
+            const signUpDto: SignUpDto = {
+                emailId: 'test@example.com',
+                password: 'password',
+                name: 'Test User',
+                type: UserType.STUDENT,
+            };
+
+            jest.spyOn(userRepository, 'exists').mockResolvedValue(true);
+
+            await expect(service.signUpUser(signUpDto)).rejects.toThrow(
+                ConflictException,
+            );
+        });
     });
 });
