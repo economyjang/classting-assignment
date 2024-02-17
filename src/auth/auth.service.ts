5d514a6124155595910ab8301e05b8e4e94f5d1c
Kyungjae-Jang <economyjang777@gmail.com>
2024-02-18 00:31:11 +0900

    feat: 로그인 서비스 로직, 컨트롤러 엔드포인트 작성
    - 로그인 함수와 컨트롤러 엔드포인트 연결
    - 로그인 서비스 로직 개발
    - 존재하는 사용자 인지 여부 검증
    - 패스워드 검증
    - 올바른 사용자일시 jwt 토큰 발급

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,21 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+    ConflictException,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/User.entity';
 import { Repository } from 'typeorm';
 import { SignUpDto } from './dto/SignUpDto';
 import bcrypt from 'bcrypt';
+import { LoginDto } from './dto/LoginDto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(User) private userRepository: Repository<User>,
+        private jwtService: JwtService,
     ) {}
 
     async signUpUser(signUpDto: SignUpDto) {
@@ -26,6 +33,32 @@ export class AuthService {
             .setType(signUpDto.type);
 
         await this.userRepository.save(user);
+    }
+
+    async loginUser(loginDto: LoginDto) {
+        const user = await this.userRepository.findOne({
+            where: { email_id: loginDto.emailId },
+        });
+        if (!user) {
+            throw new UnauthorizedException('존재하지 않는 계정입니다.');
+        }
+
+        const isValidPassword = await this.comparePasswords(
+            loginDto.password,
+            user.password,
+        );
+        if (!isValidPassword) {
+            throw new UnauthorizedException('패스워드가 일치하지 않습니다.');
+        }
+
+        const payload = {
+            emailId: user.email_id,
+            name: user.name,
+            type: user.type,
+        };
+        return {
+            accessToken: this.jwtService.sign(payload),
+        };
     }
 
     private async hashPassword(password: string): Promise<string> {
