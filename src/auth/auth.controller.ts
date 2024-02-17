cd625d2e2aadd2270ecf137f8c1bb3e8cb31d2c3
Kyungjae-Jang <economyjang777@gmail.com>
2024-02-17 23:36:57 +0900

    feat: signup 엔드포인트 작성 및 서비스 레이어와 연결

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { SignUpDto } from './dto/SignUpDto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    @HttpCode(200)
+    async signUp(@Body() signUpDto: SignUpDto) {
+        await this.authService.signUpUser(signUpDto);
+    }
+}
