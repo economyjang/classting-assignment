fbdcae93659add6581acd873a8b842aade8befe9
Kyungjae-Jang <economyjang777@gmail.com>
2024-02-18 16:32:44 +0900

    fix : 모듈 이름 변경에 따른 삭제

--- src/school/school.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SchoolService } from './school.service';
-import { SchoolController } from './school.controller';
-
-@Module({
-  providers: [SchoolService],
-  controllers: [SchoolController]
-})
-export class SchoolModule {}
